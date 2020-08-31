c0265b71ed618b157c2bfef04a0d7662b08e6c09
Malik DeJean <mdejean@MacBook-Pro.local>
2020-08-31 12:32:40 -0700

    setting state

--- google-books/src/App.js
+++ google-books/src/App.js
@@ -1,27 +1,42 @@
 import React from 'react';
 
-const base_URL= `https://www.googleapis.com/books/v1/volumes?q=`
+const base_URL = `https://www.googleapis.com/books/v1/volumes?q=`
 
 class App extends React.Component {
 
-  state={
-    items: []
-  }
-
-  componentDidMount(){
-  fetch( base_URL+"harry-potter")
-  .then(res => res.json())
-  .then(data=> this.setState({items: data.items[0].volumeInfo.title}) )
+  state = {
+    items: [],
+    volumeInfo: {},
+    title: []
 
   }
 
-  render(){
-  // const books = this.state.items.map(el => <li>{el}</li>)
-  return (
-    <main className='App'>
-      {this.state.items}
-    </main>
-  );}
+  componentDidMount() {
+    fetch(base_URL + "harry-potter")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          items: data.items,
+          volumeInfo: data.items.map(item => item.volumeInfo),
+          title: data.items.map(item => item.volumeInfo.title)
+
+        })
+      })
+  }
+
+  render() {
+    console.log('rendering...', this.state.volumeInfo)
+    const books = this.state.title.map(el => <li>{el}</li>)
+    const volume = Object.keys(this.state.volumeInfo).map((vol) => {
+     return <li>{this.state.volumeInfo.vol}</li>
+    } )
+    return (
+      <main className='App'>
+        {books}
+        {volume}
+      </main>
+    );
+  }
 
 }
 
