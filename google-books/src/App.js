83d642a316c3c3ed9c7d837acb3c1aa3647ec2f9
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-31 17:30:15 -0400

    working search bar

--- google-books/src/App.js
+++ google-books/src/App.js
@@ -1,33 +1,55 @@
 import React from 'react';
+import Search from './search'
 
 const base_URL = `https://www.googleapis.com/books/v1/volumes?q=`
+
+
+
 
 class App extends React.Component {
 
   state = {
-    items: []
+    items: [],
+    searchQuery:""
   }
-
-  componentDidMount() {
-    fetch(base_URL + "harry-potter")
+  
+  searchFunction = (title) => {
+    console.log('clicked')
+    fetch(base_URL + title)
       .then(res => res.json())
       .then(data => {
+        console.log(data.items)
         this.setState({
           items: data.items
         })
       })
+    // this.setState({searchQuery: title})
+    // this.setState({searchQuery: e.target.value})
   }
+
+  // componentDidMount() {
+  //   fetch(base_URL + this.state.searchQuery)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       this.setState({
+  //         items: data.items
+  //       })
+  //     })
+  // }
 
   render() {
     console.log('rendering...', this.state.items)
-    const books = this.state.items.map(el => <li>
+    const books =(this.state.items.length) ? this.state.items.map(el => <li key={el.id}>
       <p>{el.volumeInfo.title}</p>
     <p>{el.volumeInfo.authors}</p>
     <p>{el.saleInfo.isEbook ? 'Ebook' : 'Not EBook'}</p>
     <img src={el.volumeInfo.imageLinks.thumbnail} alt="Harry Potter"/>
-      </li>)
+      </li>) : "Enter a serach"
     return (
       <main className='App'>
+        <Search 
+        searchFunction= {this.searchFunction}
+        />
         {books}
       </main>
     );
--- google-books/src/search.js
+++ google-books/src/search.js
@@ -1,1 +1,22 @@
 import React from 'react';
+
+
+export default class Search extends React.Component{
+onSubmitForm =(e)=>{
+    e.preventDefault()
+    this.props.searchFunction(e.target.searchForm.value)
+
+}
+render(){
+return (
+<form  onSubmit={this.onSubmitForm} className="searchBox"> 
+    
+    <label>
+        Search here:
+        <input type ="text" name="searchForm" placeholder="Enter book here" ></input>
+    </label>
+    <button  type = "submit" className="search-button">Search</button>
+</form>)
+
+} 
+}
